f95d626c09be393a64e14441f76a1ea11a86abd1
Adam <adam@playnimbus.com>
2016-12-11 13:29:07 -0700

    Camera work. Meta file changes.

--- Assets/Scripts/ScreenNavigator.cs
+++ Assets/Scripts/ScreenNavigator.cs
@@ -3,50 +3,68 @@ using System.Collections;
 
 // Controls navigation between game screens
 // Swipe horizontally to switch screens or use buttons
+// Assumes x axis aligned
 [RequireComponent(typeof(Camera))]
 public class ScreenNavigator : MonoBehaviour
 {
     // Where the camera should stop to be on different screens
     public Transform[] cameraPositions;
+    public Collider activationCollider;
 
+    private Transform currentPosition;
     private new Camera camera;
     private Coroutine movementCoroutine;
 
     private void Start()
     {
+        InitializeCurrentPosition();
         camera = GetComponent<Camera>();
         StartCoroutine(NavigationCoroutine());
+    }
+
+    private void InitializeCurrentPosition()
+    {
+        float minDistance = float.PositiveInfinity;
+        foreach (var transf in cameraPositions)
+        {
+            var distance = Vector3.Distance(transform.position, transf.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                currentPosition = transf;
+            }
+        }
     }
 
     private IEnumerator NavigationCoroutine()
     {
         while(true)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
-                if (movementCoroutine != null) StopCoroutine(movementCoroutine);
-                var initialPosition = camera.ScreenToWorldPoint(Input.mousePosition).x;
-
-                while(!Input.GetMouseButtonUp(0))
+                RaycastHit hit;
+                var ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, float.PositiveInfinity) && hit.collider == activationCollider)
                 {
-                    var deltaPosition = initialPosition - camera.ScreenToWorldPoint(Input.mousePosition).x;
-                    transform.Translate(Vector3.right * deltaPosition, Space.Self);
-                    yield return null;
-                }
 
-                Transform targetPosition = null;
-                float minDistance = float.PositiveInfinity;
-                foreach(var transf in cameraPositions)
-                {
-                    var distance = Vector3.Distance(transform.position, transf.position);
-                    if (distance < minDistance)
+                    if (movementCoroutine != null)
                     {
-                        minDistance = distance;
-                        targetPosition = transf;
+                        StopCoroutine(movementCoroutine);
+                        InitializeCurrentPosition();
                     }
-                }
+                    var initialPosition = camera.ScreenToWorldPoint(Input.mousePosition).x;
 
-                movementCoroutine = StartCoroutine(SnapToPosition(targetPosition));
+                    while (!Input.GetMouseButtonUp(0))
+                    {
+                        var deltaPosition = initialPosition - camera.ScreenToWorldPoint(Input.mousePosition).x;
+                        transform.Translate(Vector3.right * deltaPosition, Space.Self);
+                        yield return null;
+                    }
+
+
+                    var targetPosition = GetNextPosition();
+                    movementCoroutine = StartCoroutine(SnapToPosition(targetPosition));
+                }
             }
 
             yield return null;
@@ -55,6 +73,8 @@ public class ScreenNavigator : MonoBehaviour
 
     private IEnumerator SnapToPosition(Transform target)
     {
+        currentPosition = target;
+
         while (Vector3.Distance(transform.position, target.position) > 0.1f)
         {
             transform.position = Vector3.Lerp(transform.position, target.position, 0.1f);
@@ -62,5 +82,33 @@ public class ScreenNavigator : MonoBehaviour
         }
 
         movementCoroutine = null;
+    }
+
+    private Transform GetNextPosition()
+    {
+        Transform targetPosition = null;
+        float minDistance = float.PositiveInfinity;
+        foreach (var transf in cameraPositions)
+        {
+            if (transf == currentPosition) continue;
+
+            var distance = Vector3.Distance(this.transform.position, transf.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                targetPosition = transf;
+            }
+        }
+
+        // targetPosition now has nearest postion aside from current
+        float diff = Vector3.Distance(targetPosition.position, currentPosition.position);
+        float distanceToCurrent = Vector3.Distance(currentPosition.position, this.transform.position);
+        bool inCorrectDirection = (this.transform.position.x - currentPosition.position.x) * (targetPosition.position.x - currentPosition.position.x) > 0;
+
+        // If we are over one quarter the way to the nearest position, go there
+        if (inCorrectDirection && distanceToCurrent / diff > 0.25f)
+            return targetPosition;
+        else
+            return currentPosition;
     }
 }
